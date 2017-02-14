875748b29de6f1b5eed670574cea28e0e47d2ee2
Lynus Vaz <lvaz@codeaurora.org>
2017-02-14 12:17:37 +0530

    msm: kgsl: Clean up device debugfs files on removal
    Remove the kgsl device-specific debugfs files in the device removal
    sequence, and also if probe fails.
    
    Change-Id: I4d5e9ec33a887f29c14bac513c4faf75266e990b
    Signed-off-by: Lynus Vaz <lvaz@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4787,6 +4787,7 @@ error_close_mmu:
 error_pwrctrl_close:
 	kgsl_pwrctrl_close(device);
 error:
+	kgsl_device_debugfs_close(device);
 	_unregister_device(device);
 	return status;
 }
@@ -4816,6 +4817,7 @@ void kgsl_device_platform_remove(struct kgsl_device *device)
 
 	kgsl_pwrctrl_close(device);
 
+	kgsl_device_debugfs_close(device);
 	_unregister_device(device);
 }
 EXPORT_SYMBOL(kgsl_device_platform_remove);
